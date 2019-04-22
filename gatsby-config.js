d1f71d6c9e131a69bc72c7b05c424b0a003ee0cf
patrickschramm <patrickschramm@gmail.com>
2019-04-21 23:17:52 -0500

    ee

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: "Mayors of Lajitas Band",
+    author: "Mayors of Lajitas",
+    description: "Border Grooves from Austin, Texas"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
